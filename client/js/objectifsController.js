e5e0618d25e0b13a9d52d4496ad4321bbfe5654c
Pygocentrus <pierre.guilhou14@gmail.com>
2014-03-27 19:21:57 +0100

    UPDATE finalize front objectifs

--- client/js/objectifsController.js
+++ client/js/objectifsController.js
@@ -1,4 +1,4 @@
-begreen.controller('objectifs', ['$scope', '$location', 'Emission', function($scope, $location, Emission) {
+begreen.controller('objectifs', ['$scope', '$location', '$q', 'Emission', function($scope, $location, $q, Emission) {
 	$scope.objectif = [];
 	$scope.objs = [];
 
@@ -10,42 +10,80 @@ begreen.controller('objectifs', ['$scope', '$location', 'Emission', function($sc
 
 	loadResource('/objectifs', function(data){
 		$.each(data, function(i, d){
-			d.status = getStatus(d.status);
+			var promise = getStatus(d);
+			promise.then(function(status){
+				d.status = status;
+				console.log('Status : '+d.status);
+			});
 		});
 		$scope.$apply(function(){
 	        $scope.objs = data;
 	    });
 	});
 
+	// Submit button
 	$('.addObj').unbind('click').on('click', function(){
-		var datas = {
-			id: $scope.objectif.id,
-			kg: $scope.objectif.kg
+		if(!isNaN($scope.objectif.kg)){
+			var datas = {
+				id: $scope.objectif.id,
+				kg: $scope.objectif.kg
+			}
+			loadResource('/objectifs/update', function(data){}, datas, 'get');
 		}
-		loadResource('/objectifs/update', function(data){}, datas, 'get');
 	});
+
+	// Sends an ajax request according to the parameters,
+	// then executes the given callback
+	function loadResource(askedUrl, callback, data, method){
+		data = typeof data !== 'undefined' ? data : null;
+		method = typeof method !== 'undefined' ? method : 'get';
+		$.ajax({
+			url: askedUrl,
+			data: data,
+			method: method
+		}).done(function objectifsGrabbed(data){
+			callback(data);
+		});
+	}
+
+	// Checks the status of the CO2 consumption
+	function getStatus(objectif){
+		var deferred = $q.defer();
+		var month = new Date(objectif.month).getMonth();
+		var year = new Date(objectif.month).getFullYear();
+		var formatedDate = year+'-'+month;
+		loadResource('totalForThisMonth/'+formatedDate, function(conso){
+			console.log('Théorique : '+objectif.kg+' - Effectif : '+Math.floor(conso[0], 2));
+			deferred.resolve(compare(objectif.kg, Math.floor(conso[0])));
+		});
+		return deferred.promise;
+	}
+
+	// Compares the gap between the estimation & reality amount of CO2
+	function compare(estimation, reality){
+		var deferred = $q.defer();
+		var eMinR = parseFloat(estimation) - parseFloat(reality);
+		var percent;
+		if(eMinR > 0){			// Better than expected
+			percent = 100*estimation/reality;
+			console.log(percent);
+			if(percent < 5) deferred.resolve('Pas mal');
+			else if(percent < 100) deferred.resolve('Vraiment Bien !');
+			else if(percent < 120) deferred.resolve('Excellent !');
+			else if(percent < 140) deferred.resolve('Quelle exemplarité !');
+			else deferred.resolve('Quelle légende, bravo !');
+		}else if(eMinR < 0){	// Worst tha expected
+			percent = Math.abs(100*estimation/reality);
+			console.log(percent);
+			if(percent < 20) deferred.resolve('Lamentable...');
+			else if(percent < 40) deferred.resolve('Catastrophique');
+			else if(percent < 60) deferred.resolve('A changer rapidement !');
+			else if(percent < 80) deferred.resolve('Peux mieux faire');
+			else deferred.resolve('Dommage');
+		}else{
+			return 'Quelle précision !';
+		}
+		return deferred.promise;
+	}
 
 }]);
-
-function loadResource(askedUrl, callback, data, method){
-	data = typeof data !== 'undefined' ? data : null;
-	method = typeof method !== 'undefined' ? method : 'get';
-	$.ajax({
-		url: askedUrl,
-		data: data,
-		method: method
-	}).done(function objectifsGrabbed(data){
-		callback(data);
-	});
-}
-
-function getStatus(status){
-	switch(status){
-		case 1: return 'Très bien'; break;
-		case 2: return 'Bonne estimation'; break;
-		case 3: return 'Vraiment pas mal'; break;
-		case 4: return 'Estimation médiocre'; break;
-		case 5: return 'Mauvaise estimation'; break;
-		default: return 'Pas mal'; break;
-	}
-}
